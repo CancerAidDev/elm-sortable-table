c84d5eed745e893dedfd83c584022a898b68febe
Dominic Endrei <dominic.endrei@gmail.com>
2022-05-17 16:33:06 +1000

    Remove

--- examples/elements.js
+++ examples/elements.js
@@ -1,5 +1,3 @@
 import { Elm } from "./Elements.elm";
 
-console.log(Elm);
-
 Elm.Elements.init({ node: document.getElementById("root") });
--- examples/travel.js
+++ examples/travel.js
@@ -1,5 +1,3 @@
 import { Elm } from "./Travel.elm";
 
-console.log(Elm);
-
 Elm.Travel.init({ node: document.getElementById("root") });
